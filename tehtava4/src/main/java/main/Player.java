827b347d05decc8aae19704ad24085d46836221b
Vili.H <vili.hautakoski@gmail.com>
2025-02-02 16:30:14 +0200

    Versio4

--- tehtava4/src/main/java/main/Player.java
+++ tehtava4/src/main/java/main/Player.java
@@ -12,7 +12,7 @@ public class Player implements Serializable {
 
     public void attack(Monster target) {
         System.out.println(name+" hyökkää "+target.type+" hirviöön!");
-        System.out.println(target.type+" on "+target.health+" elämää jäljellä.");
+        System.out.println("Hirviöllä on "+target.health+" elämää jäljellä.");
     }
 }
 
